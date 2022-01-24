eb59ecbbd6c39970ee9208a798463e7facafe0f4
StefanBoca <stefan.r.boca@gmail.com>
2022-01-24 07:41:35 -0800

    remove freertos

--- src/flip_clock.ino
+++ src/flip_clock.ino
@@ -1,16 +1,12 @@
-// FreeRTOS - Version: Latest
 // IRremote - Version: Latest
 // AccelStepper - Version: Latest
 // SevSeg - Version: Latest
 
 // #define DEBUG_IR
-
-#define INCLUDE_xSemaphoreGetMutexHolder 1
+#define BAUD 115200
 
 #include <AccelStepper.h>
-#include <Arduino_FreeRTOS.h>
 #include <SevSeg.h>
-#include <semphr.h>
 
 /***** TINY RECEIVER MACROS *****/
 // These need to come before the TinyIRReceiver include
@@ -32,7 +28,6 @@
 
 AccelStepper steppers[]
     = {AccelStepper(AccelStepper::FULL4WIRE, 22, 26, 24, 28), AccelStepper(AccelStepper::FULL4WIRE, 23, 27, 25, 29)};
-SemaphoreHandle_t stepperMutex;
 
 /***** DISPLAY CONSTANTS *****/
 
@@ -41,17 +36,15 @@ const byte DIGIT_PINS[NUM_DIGITS] = {2, 3, 4, 5};
 const byte SEGMENT_PINS[8] = {6, 7, 8, 9, 10, 11, 12, 13};
 
 SevSeg sevSeg;
-SemaphoreHandle_t sevSegMutex;
 
 /***** IR INTERRUPT ISR *****/
-
-QueueHandle_t irQueue;
 
 struct IrData {
     uint16_t address;
     uint16_t command;
     bool isRepeat;
-};
+} irData = {0, 0, 0};
+bool irReady = 0;
 
 void handleReceivedTinyIRData(uint16_t address, uint8_t command, bool isRepeat) {
 #ifdef DEBUG_IR
@@ -63,69 +56,14 @@ void handleReceivedTinyIRData(uint16_t address, uint8_t command, bool isRepeat) 
     Serial.print(isRepeat);
     Serial.println();
 #endif
-    struct IrData irData = {address, command, isRepeat};
-    xQueueSendFromISR(irQueue, &irData, NULL);
-}
-
-/***** TASKS *****/
-
-void TaskHandleInput(void* pvParameters) {
-    (void)pvParameters;
-
-    IrData irData;
-    for (;;) {
-        if (xQueueReceive(irQueue, &irData, portMAX_DELAY) > 0) {
-            if (irData.address == 0 && irData.isRepeat == 0) {
-                if (xSemaphoreTake(sevSegMutex, 10) == pdTRUE) {
-                    switch (irData.command) {
-                        case 0x16: /*0*/ sevSeg.setNumber(0); break;
-                        case 0x0C: /*1*/ sevSeg.setNumber(1); break;
-                        case 0x18: /*2*/ sevSeg.setNumber(2); break;
-                        case 0x5E: /*3*/ sevSeg.setNumber(3); break;
-                        case 0x08: /*4*/ sevSeg.setNumber(4); break;
-                        case 0x1C: /*5*/ sevSeg.setNumber(5); break;
-                        case 0x5A: /*6*/ sevSeg.setNumber(6); break;
-                        case 0x42: /*7*/ sevSeg.setNumber(7); break;
-                        case 0x52: /*8*/ sevSeg.setNumber(8); break;
-                        case 0x4A: /*9*/
-                            sevSeg.setNumber(9);
-                            break;
-                            //     case 0x45: /*power*/ toggleDisplayPower(); break;
-                    }
-                    xSemaphoreGive(sevSegMutex);
-                }
-            }
-        }
-
-        vTaskDelay(10);
-    }
-}
-
-void TaskUpdate(void* pvParameters) {
-    (void)pvParameters;
-
-    for (;;) {
-        // Run steppers
-        if (xSemaphoreTake(stepperMutex, 0) == pdTRUE) {
-            for (auto& s : steppers) {
-                if (s.distanceToGo() == 0) s.moveTo(-s.currentPosition());
-                s.run();
-            }
-            xSemaphoreGive(stepperMutex);
-        }
-
-        // Update display
-        if (xSemaphoreTake(sevSegMutex, 0) == pdTRUE) {
-            sevSeg.refreshDisplay();
-            xSemaphoreGive(sevSegMutex);
-        }
-    }
+    irData = {address, command, isRepeat};
+    irReady = 1;
 }
 
 /***** MAIN FUNCTIONS *****/
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(BAUD);
     initPCIInterruptForTinyReceiver();
 
     // Set all SS pins to output
@@ -137,27 +75,37 @@ void setup() {
         s.setAcceleration(STEPPER_ACCELERATION);
         s.moveTo(STEPS_PER_REV * 2.5);
     }
-
-    // TODO: create everything statically
-    stepperMutex = xSemaphoreCreateMutex();
-    sevSegMutex = xSemaphoreCreateMutex();
-    irQueue = xQueueCreate(4, sizeof(IrData));
-
-    xTaskCreate(TaskHandleInput,
-                "HandleInput",
-                128, // Stack size
-                NULL,
-                3, // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-                NULL);
-
-    xTaskCreate(TaskUpdate,
-                "Update",
-                128, // Stack size
-                NULL,
-                1, // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-                NULL);
 }
 
 void loop() {
-    // Do nothing, everything is handled by tasks
+    // Read IR remote
+    if (irReady) {
+        if (irData.address == 0 && irData.isRepeat == 0) {
+            switch (irData.command) {
+                case 0x16: /*0*/ sevSeg.setNumber(0); break;
+                case 0x0C: /*1*/ sevSeg.setNumber(1); break;
+                case 0x18: /*2*/ sevSeg.setNumber(2); break;
+                case 0x5E: /*3*/ sevSeg.setNumber(3); break;
+                case 0x08: /*4*/ sevSeg.setNumber(4); break;
+                case 0x1C: /*5*/ sevSeg.setNumber(5); break;
+                case 0x5A: /*6*/ sevSeg.setNumber(6); break;
+                case 0x42: /*7*/ sevSeg.setNumber(7); break;
+                case 0x52: /*8*/ sevSeg.setNumber(8); break;
+                case 0x4A: /*9*/
+                    sevSeg.setNumber(9);
+                    break;
+                    //     case 0x45: /*power*/ toggleDisplayPower(); break;
+            }
+        }
+        irReady = 0;
+    }
+
+    // Run steppers
+    for (auto& s : steppers) {
+        if (s.distanceToGo() == 0) s.moveTo(-s.currentPosition());
+        s.run();
+    }
+
+    // Refresh display
+    sevSeg.refreshDisplay();
 }
